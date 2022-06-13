bc2e58161ce5d0b1527f907f0e12b35f842d1aaa
pavneet-kaur1 <pavneet-kaur1@mysenecacollege.ca>
2022-06-12 23:35:09 -0400

    completed

--- blog-service.js
+++ blog-service.js
@@ -56,9 +56,113 @@ getCategories = () => {
     }
   });
 };
+addPost = (postData) => {
+  return new Promise(function(resolve,reject){
+    
+     
+    try{
+
+     postData.published=(postData.published)?true:false;
+     postData.id=posts.length+1;
+      posts.push(postData);
+     resolve(postData);
+    
+  }catch(err){
+    reject();
+  }
+
+
+
+});
+}
+getPostsByCategory = (category) => {
+  
+{
+  return new Promise(function(resolve,reject){
+    var category_posts=[];
+  
+    var j=0;
+
+    
+        for (var i=0;i<posts.length;i++)
+         {
+               if (posts[i].category==category)
+                {
+                  category_posts[j++]=posts[i];
+                }
+                
+          }
+         if(category_posts.length==0)
+         {
+           reject("no results returned");
+         }
+         else{   
+          resolve(category_posts);}
+  
+    
+  });
+}
+}
+getPostsByMinDate = (minDateStr) => {
+   
+  {
+  return new Promise(function(resolve,reject){
+    var date_posts=[];
+  
+    var j=0;
+
+    
+        for (var i=0;i<posts.length;i++)
+         {
+               if(new Date(posts[i].postDate) >= new Date(minDateStr))
+                {
+                  date_posts[j++]=posts[i];
+                }
+                
+          }
+         if(date_posts.length==0)
+         {
+           reject("no results returned");
+         }
+         else{   
+          resolve(date_posts);}
+  
+    
+  });
+}
+}
+getPostById = (id) => {
+    return new Promise((resolve,reject)=>{
+       var id_posts=[];
+       var j=0;
+        for(var i=0;i<posts.length;i++)
+        {
+            if(posts[i].id==id)
+            {
+              id_posts[j++]=posts[i];
+            }
+        }
+
+        if(id_posts.length==0)
+        {
+        reject("no results returned");
+        }   
+        else
+        {
+          resolve(id_posts);
+        } 
+        
+});
+
+ } 
 module.exports = {
     initialize,
     getAllPosts,
     getPublishedPosts,
-    getCategories
+  getCategories,
+  addPost,
+  getPostsByCategory,
+  getPostsByMinDate,
+  getPostById 
+    
 }
